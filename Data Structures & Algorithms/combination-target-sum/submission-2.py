01d10a9066618c89da838049dfe6fd2030f838a5
Aryam Shinde <aryamshinde2@gmail.com>
2026-04-03 18:39:29 -0500

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,31 @@
+'''
+[2,5,6,9]
+
+'''
+
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = [] # []
+        path = [] # [2, 2, 2, 2]
+
+        def dfs(start, total): #(0, 8)
+            if total == target: # 8 != 9
+                res.append(path.copy())
+                return
+
+            if total > target: # 8 !> 9
+                return
+            
+            for i in range(start, len(nums)):
+                if total + nums[i] > target: # 8 + 2 > 9
+                    return
+
+                path.append(nums[i])
+                dfs(i, total + nums[i])
+                path.pop()
+
+        dfs(0, 0)
+        return res
+
+        
