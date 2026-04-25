4a59669e4da7d779df3235528f0a2539d3b36b08
Aryam Shinde <aryamshinde2@gmail.com>
2026-04-25 18:09:31 -0500

    Add: n-queens-ii - submission-1

---
+++ Data Structures & Algorithms/n-queens-ii/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = 0
+        board = [["."]* n for _ in range(n)]
+
+        cols = set()
+        pos_diag = set()
+        neg_diag = set()
+
+        def dfs(r):
+            nonlocal res
+            if r == n:
+                res += 1
+                return
+
+            for c in range(n):
+                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
+                    continue
+
+                cols.add(c)
+                pos_diag.add(r + c)
+                neg_diag.add(r - c)
+
+                dfs(r + 1)
+
+                cols.remove(c)
+                pos_diag.remove(r + c)
+                neg_diag.remove(r - c)
+
+        dfs(0)
+        return res
+                
